98da6b2bd9f82c0163416eb245015f13e0b3549e
Nicolas Conguisti <nicolas.conguisti1@gmail.com>
2024-12-21 16:09:29 +0100

    feat : Ajout tu téléchargement de vidéos via FTP. Il est desormais possible de le lier à l'algo de synchro / reconciliation

--- racine/fonctions/ftp.php
+++ racine/fonctions/ftp.php
@@ -1,26 +1,33 @@
 <?php
 
-
 /**
  * Fonction qui établit une connexion FTP.
  * Prend en paramètre : nom du serveur / login / password (ex : NAS_H264, user2, pass2)
  * Retourne $conn_id. Il sera nécessaire de fermer la connexion avec "ftp_close($conn_id)"
  */
 function connexionFTP_NAS($ftp_server, $ftp_user, $ftp_pass){
-
     $conn_id = ftp_connect($ftp_server);
-
     if (!$conn_id) {
         die("Impossible de se connecter au serveur FTP : $ftp_server\n");
     }
-
     if (!ftp_login($conn_id, $ftp_user, $ftp_pass)) {
         die("Échec de la connexion pour l'utilisateur $ftp_user\n");
     }
-
     echo "Connexion réussie à $ftp_server\n";
-    
     return $conn_id;
+}
+
+/**
+ * Fonction qui télécharge un fichier dans un répertoire local
+ * Prend en paramètre l'id de connexion, le fichier à obtenir en local et le fichier sutué dans le NAS
+ */
+function telechargerFichier($conn_id, $local_file, $ftp_file){
+    if (ftp_get($conn_id, $local_file, $ftp_file, FTP_BINARY)) {
+        echo "Le fichier a été téléchargé avec succès.\n";
+    }
+    else {
+        echo "Échec du téléchargement du fichier.\n";
+    }
 }
 
 ?>
--- racine/pages/home.php
+++ racine/pages/home.php
@@ -1,10 +1,18 @@
 <?php 
 
+#$ftp_file = '/home/ftpusers/user1/23_6h_GRUNT.mp4';  // NE MARCHE PAS
+$ftp_file = '23_6h_GRUNT.mp4';
+$local_file = '../videos/videosAAnalyser/23_6h_GRUNT.mp4';
+
+
 require '../fonctions/ftp.php';
 
-echo "coucou voici la page d'accueil";
+echo "Page d'accueil, connexion au NAS ARCH  <br>";
 
 $conn_id = connexionFTP_NAS("NAS_ARCH", "user2", "pass2");
+
+telechargerFichier($conn_id, $local_file,  $ftp_file);
+
 ftp_close($conn_id);
 
 ?>
