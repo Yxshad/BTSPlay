86ac24e60470b9cfa47b7f73a79346218374a3c8
Nicolas Conguisti <nicolas.conguisti1@gmail.com>
2025-01-04 00:44:49 +0100

    feat : Lecture vidéo sans plyr

--- racine/pages/video.php
+++ racine/pages/video.php
@@ -9,14 +9,18 @@
     <script src="../ressources/Script/scripts.js"></script>
 
     <link rel="stylesheet" href="https://unpkg.com/swiper@10/swiper-bundle.min.css" />
+    <link rel="stylesheet" href="https://cdn.plyr.io/3.7.8/plyr.css" />
     <script src="https://unpkg.com/swiper@10/swiper-bundle.min.js"></script>
+    <script src="https://cdn.plyr.io/3.7.8/plyr.polyfilled.js"></script>
 
 <?php include '../ressources/Templates/header.php';?>
 
 <div class="container">
     <div class="lecteurVideo">
-        <video src=""></video>
-    </div>
+    <video class="player" id="player" playsinline controls>
+        <source src="../videos/bobo.mp4" type="video/mp4"/>
+    </video>
+</div>
     <h1 class="titre">Titre de la video</h1>
     <div class="colonnes">
         <div class="colonne-1">
@@ -55,5 +59,35 @@
             </a>
         </div>
     </div>
-   
 </div>
+
+<script>
+document.addEventListener('DOMContentLoaded', () => {
+      const player = new Plyr('#player', {
+        controls: [
+          'play-large', // The large play button in the center
+          'restart', // Restart playback
+          'rewind', // Rewind by the seek time (default 10 seconds)
+          'play', // Play/pause playback
+          'fast-forward', // Fast forward by the seek time (default 10 seconds)
+          'progress', // The progress bar and scrubber for playback and buffering
+          'current-time', // The current time of playback
+          'duration', // The full duration of the media
+          'mute', // Toggle mute
+          'volume', // Volume control
+          'captions', // Toggle captions
+          'settings', // Settings menu
+          'pip', // Picture-in-picture (currently Safari only)
+          'airplay', // Airplay (currently Safari only)
+          'download', // Custom download button
+          'fullscreen' // Toggle fullscreen
+        ],
+        settings: ['captions', 'quality', 'speed', 'loop'],
+        captions: {
+          active: true,
+          language: 'fr', // Default language
+          update: true,
+        },
+      });
+    });
+<script>
