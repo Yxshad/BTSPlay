3cb6e1dd6a6b1e24ef897b27bd88eaf4e4676f8f
Nicolas Conguisti <124582837+Nicolas-Conguisti@users.noreply.github.com>
2025-02-07 10:04:29 +0100

    Update home.php corrections DEV_Julien

--- racine/pages/home.php
+++ racine/pages/home.php
@@ -13,6 +13,7 @@ $tabVideos = controleurRecupererTitreIdVideo();
     <link href="../ressources/Style/main.css" rel="stylesheet">
     <link href="../ressources/Style/menuFiltres.css" rel="stylesheet">
     <link href="../ressources/Style/home.css" rel="stylesheet">
+    <script src="../ressources/Script/script.js"></script>
 
     <link href="../ressources/lib/Swiper/swiper-bundle.min.css" rel="stylesheet">
     <script src="../ressources/lib/Swiper/swiper-bundle.min.js"></script>
