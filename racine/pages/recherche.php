207c7394f4ce5607a48b3ab6942cf179be7597e1
Nicolas Conguisti <124582837+Nicolas-Conguisti@users.noreply.github.com>
2025-02-07 10:15:46 +0100

    Update recherche.php

--- racine/pages/recherche.php
+++ racine/pages/recherche.php
@@ -1,36 +1,35 @@
-<?php
-require_once '../fonctions/controleur.php';
+<?php session_start(); ?>
+<!DOCTYPE html>
+<html lang="fr">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <link rel="icon" href="../ressources/Images/favicon_BTS_Play.png" type="image/png">
+    <link href="../ressources/Style/main.css" rel="stylesheet">
+    <link href="../ressources/Style/menuFiltres.css" rel="stylesheet">
+    <link href="../ressources/Style/recherche.css" rel="stylesheet">
+    <script src="../ressources/Script/script.js"></script>
 
-$annee = isset($_POST['annee']) ? intval($_POST['annee']) : null;
-$niveau = isset($_POST['niveau']) ? intval($_POST['niveau']) : null;
-$prof = isset($_POST['prof']) ? $_POST['prof'] : null;
+<?php require_once '../ressources/Templates/header.php'; ?>
 
-$tabVideos = controleurRecupererTitreIdVideoFiltre($annee, $niveau, $prof); // Nouvelle fonction filtrée
+<?php require_once '../ressources/Templates/menuFiltres.php'; ?>
 
-if (!empty($tabVideos)) {
-    echo '<div class="sliderVideo">';
-    echo '<h2>Vos vidéos</h2>';
-    echo '<div class="swiperVideo">';
-    echo '<div class="swiper-wrapper">';
-    foreach ($tabVideos as $video) {
-        $id = $video['id'];
-        $titre = $video['mtd_tech_titre'];
-        $cheminMiniature = URI_RACINE_STOCKAGE_LOCAL . $video['URI_STOCKAGE_LOCAL'] . trouverNomMiniature($titre);
-        echo "<div class='swiper-slide'>";
-        echo "<a href='video.php?v=$id'>";
-        echo "<div class='miniature'>";
-        echo "<img src='$cheminMiniature' alt='Miniature' class='imageMiniature'/>";
-        echo "</div>";
-        echo "<h3>$titre</h3>";
-        echo "</a>";
-        echo "</div>";
-    }
-    echo '</div>';
-    echo '</div>';
-    echo '<div class="swiper-button-next"></div>';
-    echo '<div class="swiper-button-prev"></div>';
-    echo '</div>';
-} else {
-    echo "<p>Aucune vidéo trouvée.</p>";
-}
-?> 
+<div class="container">
+    <?php for ($i=0; $i < 5; $i++) { ?>
+        <a href="video.php" class="video">
+            <div class="miniature"></div>
+            <div class="description">
+                <h2 class="titre">Video <?php echo $i; ?></h2>
+                <p class="description"> Métadonnées : Lorem Ipsum</p>
+            </div>
+        </a>
+    <?php } ?>
+</div>
+
+<?php require_once '../ressources/Templates/footer.php'; ?>
+
+<script>
+    document.addEventListener("DOMContentLoaded", function () {
+        affichageFiltres();
+    });
+</script>
