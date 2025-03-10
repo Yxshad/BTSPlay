4a9f6c91be75ecf7734686d89109841dbb95bffa
Marrier-Axel <marrieraxel@gmail.com>
2025-03-10 14:42:29 +0100

    fix: ajout d'un commentaire pour montrer l'utilisation de la commande des popup

--- racine/ressources/Templates/popup.php
+++ racine/ressources/Templates/popup.php
@@ -14,3 +14,29 @@
 <script>
     boutonsPopUp(JSON.parse('<?php echo json_encode($btn1)?>'), JSON.parse('<?php echo json_encode($btn2); ?>'));
 </script>
+
+<!--
+
+Pour faire appartaitre la popup, il faut utiliser en php la fonction controleurPopUp($titre, $explication, $btn1, $btn2)
+$titre et $explications sont des strings.
+
+$btn1 et $btn2 ont une structure qui ressemble à ça : 
+
+$btn1 = [
+    "libelle" => "Oui!",
+    "arguments" => [["action","supprimerVideo"], ["idvideo", "5"]]
+];
+
+Libelle sera le texte affiché sur le bouton.
+arguments est une liste de paire de string, une paire correspond au nom de la variable envoyé en post et le deuxième est la valeur de la variable.
+Dans cette exemple, le $btn1 envera dans le fichier controleur.php les variables $_POST['action'] = 'supprimerVideo' et $_POST['idvideo'] = '5'
+
+Si l'appel de la popup doit se faire après l'action de l'utilisateur, on peut faire la même chose avec du js avec l'attribut onclick
+alors, la déclaration de btn1 se fera comme ça:
+
+let btn1 = {
+    libelle : "Oui!",
+    arguments : [["action","supprimerVideo"], ["idvideo", "5"]]
+}
+
+-->
