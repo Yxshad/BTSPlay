c0b4330b58a670951b7e2e42b1bf245d08f5b73a
LORIDANT Julien <jloridant@cdg33.fr>
2025-03-12 14:24:26 +0100

    mise à jour du css de video.php

--- racine/pages/video.php
+++ racine/pages/video.php
@@ -36,28 +36,57 @@
     <script src="../ressources/lib/Plyr/plyr.js"></script>
     <link rel="stylesheet" href="../ressources/lib/Plyr/plyr.css" />
 
-<?php require_once '../ressources/Templates/header.php';?>
+    <?php require_once '../ressources/Templates/header.php';?>
 
-<div class="container">
-    <div class="lecteurVideo">
-        <video class="player" id="player" playsinline controls data-poster=<?php echo $cheminMiniatureComplet; ?>>
-            <source src="<?php echo $cheminVideoComplet; ?>" type="video/mp4"/>
-        </video>
-    </div>
-    <h1 class="titre"><?php echo $nomFichier; ?></h1>
-    <h2 ><?php echo $titreVideo; ?></h2>
+    <div class="contenu">
+        <div class="container_principal">
+            <div class="container_video">
+                <div class="lecteurVideo">
+                    <video class="player" id="player" playsinline controls data-poster=<?php echo $cheminMiniatureComplet; ?>>
+                        <source src="<?php echo $cheminVideoComplet; ?>" type="video/mp4"/>
+                    </video>
+                </div>
+            </div>
+            <div class="info_video">
 
+                <div class ="titre_nom">
+                    <h1 class="titre"><?php echo $nomFichier; ?></h1>
+                    <h2 ><?php echo $titreVideo; ?></h2>
+                </div>
 
+                <div class="container-button">
+                    <!-- Bouton Télécharger -->
+                    <button title="Télécharger vidéo" class="btnVideo" onclick="window.location.href='<?php echo $cheminVideoComplet; ?>';">
+                        <div class="logo-btnvideo">
+                            <img src="../ressources/Images/télécharger_image_blanc.png" alt="">
+                        </div>
+                    </button>
 
+                    <?php if(controleurVerifierAcces(ACCES_DIFFUSION)){ ?>
+                        <?php if(!empty($cheminCompletNAS_PAD)){ ?>
+                            <button class="btnVideo" onclick="afficherPopUp('Diffusion', 'Voulez-vous vraiment diffuser la vidéo <?php echo htmlspecialchars($nomFichier); ?> ?', {libelle : 'Oui!', arguments : [['action','diffuserVideo'], ['URI_COMPLET_NAS_PAD', '<?php echo htmlspecialchars($cheminCompletNAS_PAD); ?>']]}, {libelle : 'Non!', arguments : []})"></button>
+                        <?php }
+                    }
+                    if(controleurVerifierAcces(ACCES_MODIFICATION)){ ?>
+                        <button title="Modifier vidéo" class="btnVideo" onclick="window.location.href='formulaireMetadonnees.php?v=<?php echo $idVideo; ?>';">
+                            <div class="logo-btnvideo">
+                                <img src="../ressources/Images/modifier_video_blanc.png" alt="">
+                            </div>
+                        </button>
+                    <?php }
+                    if(controleurVerifierAcces(ACCES_SUPPRESSION)){ ?>             
+                        <button title="Supprimer vidéo" class="btnVideo" id="btnSuppr" onclick="afficherPopUp('Suppression', 'Voulez-vous vraiment Supprimer la vidéo <?php echo htmlspecialchars($nomFichier); ?> ?', {libelle : 'Oui!', arguments : [['action','supprimerVideo'], ['idVideo', '<?php echo htmlspecialchars($idVideo); ?>'], ['URI_STOCKAGE_LOCAL', '<?php echo $cheminVideoComplet; ?>']]}, {libelle : 'Non!', arguments : []})">
+                            <p>
+                                supprimer
+                            </p>
+                        </button>
+                    <?php } ?>
+                </div>
 
+            </div>
+        </div>
 
-
-
-
-
-
-    <div class="colonnes">
-        <div class="colonne-1">
+        <div class="metadata_detaillee"  >
             <p class="description"><?php echo $mtdTech["Description"]; ?></p>
             <p class="mtd">
                 <strong>URI du NAS PAD : </strong><?php echo $URIS['URI_NAS_PAD']; ?>
@@ -95,43 +124,9 @@
             <p class="mtd">
                 <strong>Responsable Son : </strong><?php echo $mtdEdito["responsableSon"]; ?>
             </p>
-            
-        </div>
-        <div class="colonne-2">
-            <!-- #RISQUE : Télécharger le fichier distant du NAS ARCH, il faudra créer un dossier local 'videosATelecharger' -->
-            <a href="<?php echo $cheminVideoComplet; ?>" download="<?php echo $mtdTech["mtd_tech_titre"]; ?>" class="btnVideo">
-                <div class="logo-btnvideo">
-                    <img src="../ressources/Images/download.webp" alt="">
-                </div>
-                <p>Télécharger</p>
-            </a>
-            <?php if(controleurVerifierAcces(ACCES_DIFFUSION)){ ?>
-                <?php if(!empty($cheminCompletNAS_PAD)){ ?>
-                <div class="btnVideo">
-                    <button onclick="afficherPopUp('Diffusion', 'Voulez-vous vraiment diffuser la vidéo <?php echo htmlspecialchars($nomFichier); ?> ?', {libelle : 'Oui!', arguments : [['action','diffuserVideo'], ['URI_COMPLET_NAS_PAD', '<?php echo htmlspecialchars($cheminCompletNAS_PAD); ?>']]}, {libelle : 'Non!', arguments : []})">
-                        Diffuser
-                    </button>
-                </div>
-                <?php }
-            }
-            if(controleurVerifierAcces(ACCES_MODIFICATION)){ ?>
-                <a href="formulaireMetadonnees.php?v=<?php echo $idVideo; ?>" class="btnVideo">
-                    <div class="logo-btnvideo">
-                        <img src="../ressources/Images/modif.png" alt="">
-                    </div>
-                    <p>Modifier</p>
-                </a>
-            <?php }
-            if(controleurVerifierAcces(ACCES_SUPPRESSION)){ ?>             
-                <div class="btnVideo">
-                    <button onclick="afficherPopUp('Suppression', 'Voulez-vous vraiment Supprimer la vidéo <?php echo htmlspecialchars($nomFichier); ?> ?', {libelle : 'Oui!', arguments : [['action','supprimerVideo'], ['idVideo', '<?php echo htmlspecialchars($idVideo); ?>'], ['URI_STOCKAGE_LOCAL', '<?php echo $cheminVideoComplet; ?>']]}, {libelle : 'Non!', arguments : []})">
-                        Supprimer
-                    </button>
-                </div>
-            <?php } ?>
         </div>
     </div>
-</div>
+
 
 <?php require_once '../ressources/Templates/footer.php'; ?>
 
