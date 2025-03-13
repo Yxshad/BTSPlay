1dc5c62a44d9a2f9e1d6cb4c32fa6bf8bfaeac7a
Solene <solenelina@gmail.com>
2025-03-13 19:37:34 +0100

    fix: bouton Terminer en Retour

--- racine/pages/formulaireMetadonnees.php
+++ racine/pages/formulaireMetadonnees.php
@@ -98,7 +98,7 @@
     </div>
 
     <div class="btns">
-        <a href="video.php?v=<?php echo $idVideo; ?>" class="btn">Terminer</a>
+        <a href="video.php?v=<?php echo $idVideo; ?>" class="btn">Retour</a>
     </div>
 </div>
 
