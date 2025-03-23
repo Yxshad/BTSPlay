65a128071b61311e75506e5d35214bd30844f954
Nicolas Conguisti <nicolas.conguisti1@gmail.com>
2025-03-24 00:25:55 +0100

    clean code

--- racine/fonctions/ffmpeg.php
+++ racine/fonctions/ffmpeg.php
@@ -98,8 +98,7 @@ function traiterVideo($titre, $duree) {
 
     $chemin_dossier_conversion = URI_VIDEOS_A_UPLOAD_EN_COURS_DE_CONVERSION . $titre . '_parts';
 
-    ajouterLog(LOG_CRITICAL, "Création du dossier : $chemin_dossier_conversion.");
-    creerDossier($chemin_dossier_conversion, false, false);
+    creerDossier($chemin_dossier_conversion, false);
 
     // Vérifier si la durée totale est inférieure à 100 secondes
     if ($total < 100) {
