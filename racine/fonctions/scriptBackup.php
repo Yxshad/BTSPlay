4b7e5fcf2d1b9b0ef487ce2e0ee52eadc17724d4
Yxshad <elavergne001@iutbayonne.univ-pau.fr>
2025-04-17 21:54:24 +0200

    fix: sauvegarde plus stable

--- racine/fonctions/scriptBackup.php
+++ racine/fonctions/scriptBackup.php
@@ -5,6 +5,6 @@
 
     $backupDir = "/var/www/html/ressources/datas/dumpBD";
     $fileName = $backupDir . "/autosave_" . date("j-m-Y_H-i-s_") . SUFFIXE_FICHIER_DUMP_SAUVEGARDE;
-    $commandSql = 'mysqldump --user='.BD_USER.' --password='.BD_PASSWORD.' --host=mysql '.BD_NAME.' > '.$fileName;
+    $commandSql = 'mysqldump --user='.BD_USER.' --password='.BD_PASSWORD.' --host=mysql --add-drop-table '.BD_NAME.' > '.$fileName;
     $operationSucces = exec($commandSql);
 ?>
